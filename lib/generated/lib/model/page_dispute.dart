a5ae12417e30a549a3abeb0bc1c18fb6ac1b75ab
Redandan <coconut0989@gmail.com>
2025-11-16 23:47:35 +0800

    refactor: Update API models and swagger.yaml for property adjustments and enhancements
    - Adjusted property definitions in `CartItem`, `Page*` models, and `Product` for improved clarity and consistency.
    - Enhanced handling of nullable types across various models to improve readability and maintainability.
    - Updated `swagger.yaml` to reflect the latest API structure and ensure accurate documentation.

--- lib/generated/lib/model/page_dispute.dart
+++ lib/generated/lib/model/page_dispute.dart
@@ -13,8 +13,8 @@ part of openapi.api;
 class PageDispute {
   /// Returns a new [PageDispute] instance.
   PageDispute({
-    this.totalPages,
     this.totalElements,
+    this.totalPages,
     this.sort,
     this.numberOfElements,
     this.pageable,
@@ -32,7 +32,7 @@ class PageDispute {
   /// source code must fall back to having a nullable type.
   /// Consider adding a "default:" property in the specification file to hide this note.
   ///
-  int? totalPages;
+  int? totalElements;
 
   ///
   /// Please note: This property should have been non-nullable! Since the specification file
@@ -40,7 +40,7 @@ class PageDispute {
   /// source code must fall back to having a nullable type.
   /// Consider adding a "default:" property in the specification file to hide this note.
   ///
-  int? totalElements;
+  int? totalPages;
 
   ///
   /// Please note: This property should have been non-nullable! Since the specification file
@@ -110,8 +110,8 @@ class PageDispute {
 
   @override
   bool operator ==(Object other) => identical(this, other) || other is PageDispute &&
-    other.totalPages == totalPages &&
     other.totalElements == totalElements &&
+    other.totalPages == totalPages &&
     other.sort == sort &&
     other.numberOfElements == numberOfElements &&
     other.pageable == pageable &&
@@ -125,8 +125,8 @@ class PageDispute {
   @override
   int get hashCode =>
     // ignore: unnecessary_parenthesis
-    (totalPages == null ? 0 : totalPages!.hashCode) +
     (totalElements == null ? 0 : totalElements!.hashCode) +
+    (totalPages == null ? 0 : totalPages!.hashCode) +
     (sort == null ? 0 : sort!.hashCode) +
     (numberOfElements == null ? 0 : numberOfElements!.hashCode) +
     (pageable == null ? 0 : pageable!.hashCode) +
@@ -138,19 +138,19 @@ class PageDispute {
     (empty == null ? 0 : empty!.hashCode);
 
   @override
-  String toString() => 'PageDispute[totalPages=$totalPages, totalElements=$totalElements, sort=$sort, numberOfElements=$numberOfElements, pageable=$pageable, first=$first, last=$last, size=$size, content=$content, number=$number, empty=$empty]';
+  String toString() => 'PageDispute[totalElements=$totalElements, totalPages=$totalPages, sort=$sort, numberOfElements=$numberOfElements, pageable=$pageable, first=$first, last=$last, size=$size, content=$content, number=$number, empty=$empty]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
-    if (this.totalPages != null) {
-      json[r'totalPages'] = this.totalPages;
-    } else {
-      json[r'totalPages'] = null;
-    }
     if (this.totalElements != null) {
       json[r'totalElements'] = this.totalElements;
     } else {
       json[r'totalElements'] = null;
+    }
+    if (this.totalPages != null) {
+      json[r'totalPages'] = this.totalPages;
+    } else {
+      json[r'totalPages'] = null;
     }
     if (this.sort != null) {
       json[r'sort'] = this.sort;
@@ -215,8 +215,8 @@ class PageDispute {
       }());
 
       return PageDispute(
-        totalPages: mapValueOfType<int>(json, r'totalPages'),
         totalElements: mapValueOfType<int>(json, r'totalElements'),
+        totalPages: mapValueOfType<int>(json, r'totalPages'),
         sort: SortObject.fromJson(json[r'sort']),
         numberOfElements: mapValueOfType<int>(json, r'numberOfElements'),
         pageable: PageableObject.fromJson(json[r'pageable']),
